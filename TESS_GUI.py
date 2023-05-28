223ff6064f0f1d14f686fd86d16867d1f05e9bea
Mat <ezofagus@gmail.com>
2023-05-28 22:10:12 +0200

    hello

--- TESS_GUI.py
+++ TESS_GUI.py
@@ -13,9 +13,14 @@ window = tk.Canvas(master=root, width=screen_x-500, height=screen_y-70, bg='whit
 window.grid(row=0, column=1, sticky='E')
 frame1 = tk.Frame(master=root, width=500, height=screen_y-70, bg='grey')
 frame1.grid(row=0, column=0, sticky='W')
+#v=Scrollbar(root, orient='vertical')
+#v.pack(side=RIGHT, fill='y')
 T = Text(master=frame1, height=13, width=58, bg='Light grey', bd=3, padx=10)
+#T = Text(master=frame1, height=13, width=58, bg='Light grey', bd=3, padx=10, yscrollcommand=v.set)
 T.place(x=5, y=screen_y-305)
 T.insert(END, 'Hello TESS')
+#v.config(command=T.yview)
+#T.pack()
 
 
 root.mainloop()
