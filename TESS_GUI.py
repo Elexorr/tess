6f500b8aa1009752e6a654a4cb7ffe08f572d0e2
Mat <ezofagus@gmail.com>
2023-05-29 17:25:14 +0200

    hello

--- TESS_GUI.py
+++ TESS_GUI.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 import lightkurve as lk
 # import numpy as np
 import matplotlib.pyplot as plt
+# from matplotlib.figure import Figure
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 root = tk.Tk()
 root.title('TESS project')
@@ -23,25 +25,25 @@ T = Text(master=frame1, height=18, width=65, bg='Light grey', bd=3, padx=10)
 T.place(x=5, y=screen_y-385)
 # T.insert(END, 'Hello TESS')
 
-obj_label = tk.Label(master=frame1, text='Object ID:', bg='grey')
+obj_label = tk.Label(master=frame1, font=('Helvetica', 10), text='Object ID:', bg='grey')
 obj_label.place(x=5, y=8)
 obj_name = tk.StringVar()
 obj_name_entered = ttk.Entry(frame1, width=20, textvariable=obj_name)
 obj_name_entered.place(x=65, y=8)
 
-author_label = tk.Label(master=frame1, text='Author:', bg='grey')
+author_label = tk.Label(master=frame1, font=('Helvetica', 10), text='Author:', bg='grey')
 author_label.place(x=5, y=35)
 author_name = tk.StringVar()
 author_name_entered = ttk.Entry(frame1, width=10, textvariable=author_name)
 author_name_entered.place(x=65, y=35)
 
-exptime_label = tk.Label(master=frame1, text='Exptime:', bg='grey')
+exptime_label = tk.Label(master=frame1, font=('Helvetica', 10), text='Exptime:', bg='grey')
 exptime_label.place(x=150, y=35)
 exptime = tk.IntVar()
 exptime_entered = ttk.Entry(frame1, width=6, textvariable=exptime)
 exptime_entered.place(x=205, y=35)
 
-sector_label = tk.Label(master=frame1, text='#:', bg='grey')
+sector_label = tk.Label(master=frame1, font=('Helvetica', 10), text='#:', bg='grey')
 sector_label.place(x=270, y=35)
 sector_num = tk.IntVar()
 sector_num_entered = ttk.Entry(frame1, width=3, textvariable=sector_num)
