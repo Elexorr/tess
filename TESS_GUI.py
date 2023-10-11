1c34bcadb4398073c84c7a428170a3c93f825794
Elexorr <ezofagus@gmail.com>
2023-10-11 06:07:37 +0200

    hello

--- TESS_GUI.py
+++ TESS_GUI.py
@@ -14,6 +14,7 @@ import auxiliary as aux
 from astroquery.mast import Catalogs
 import csv
 from PIL import Image,ImageTk
+from astropy.modeling import models, fitting
 import csvtodat
 import requests
 import pandas as pd
@@ -76,6 +77,8 @@ sector_num.place(x=340, y=8)
 
 
 def basic_search():
+    global fitted
+    fitted = False # Nebude vykreslovat poslednu zobrazenu krivku ale nanovo
     clear_JD() # Vymaze policka na ohranicovanie krivky
     search_lcf = lk.search_lightcurve(obj_name.get())
     T.insert(INSERT, '\n')
@@ -124,77 +127,91 @@ refined_search_button.place(x=220, y=35)
 global fig
 global canvas
 global ax
+global Maxmagvalue
+global Minmagvalue
+global xx
+global yy
 
+
+global fitted
+fitted = False
 
 def curve_plot():
+    global fitted
     global fig
     global lcf
     global window
     global canvas
     global ax
+    global x
+    global y
+    global xx
+    global yy
 
     window = tk.Canvas(master=root, width=screen_x - 558, height=screen_y - 50, bg='white')
     window.grid(row=0, column=1, sticky='N')
 
-    print(lcf[int(sector_num.get())])
+    if fitted == False:
 
-    x = lcf[int(sector_num.get())].time.value
-    y = lcf[int(sector_num.get())].flux.value
+        x = lcf[int(sector_num.get())].time.value
+        y = lcf[int(sector_num.get())].flux.value
 
-    xx = []
-    yy = []
-    zz = []
+        xx = []
+        yy = []
 
-    if JDstart_entered.get() != '':
-        JDstart = float(JDstart_entered.get())
-        JDend = float(JDend_entered.get())
-        for i in range (0, len(x)):
-            if JDstart < x[i] and  x[i] < JDend:
-                # print(x[i],y[i])
-                # print(float(x[i]))
-                xx.append(float(x[i]))
-                # print(float(y[i]))
-                yy.append(float(y[i]))
-    else:
-        xx = lcf[int(sector_num.get())].time.value
-        yy = lcf[int(sector_num.get())].flux
+        if JDstart_entered.get() != '':
+            JDstart = float(JDstart_entered.get())
+            JDend = float(JDend_entered.get())
+            for i in range (0, len(x)):
+                if JDstart < x[i] and  x[i] < JDend:
+                    # print(x[i],y[i])
+                    # print(float(x[i]))
+                    xx.append(float(x[i]))
+                    # print(float(y[i]))
+                    yy.append(float(y[i]))
+        else:
+            xx = lcf[int(sector_num.get())].time.value
+            yy = lcf[int(sector_num.get())].flux
 
-    # EXPERIMENTAL ROWS
-    # for i in range (0,len(yy)):
-    #     zz.append(yy[i]+0.1)
-    # EXPERIMENTAL ROWS
 
-    lcf[int(sector_num.get())].to_csv(path_or_buf='lightcurve.csv', overwrite=True)
+        # EXPERIMENTAL ROWS
+        # for i in range (0,len(yy)):
+        #     zz.append(yy[i]+0.1)
+        # EXPERIMENTAL ROWS
 
-    # txtcurve = str(obj_name.get()) + '_tess.txt'
-    # file = open(txtcurve, 'w')
-    # for row in lcf[int(sector_num.get())]:
-    #     mag = -2.5 * float(row['flux']) + 20
-    #     line = str(row['time']) + ' ' + str(mag) + ' ' + str(row['flux_err']) + '\n'
-    #     # line = str(row['time']) + ' ' + str(row['flux']) + ' ' + str(row['flux_err']) + '\n'
-    #     # print(line)
-    #     file.write(line)
-    # file.close()
+        lcf[int(sector_num.get())].to_csv(path_or_buf='lightcurve.csv', overwrite=True)
 
-    # with open('lightcurve.csv', newline='') as csvfile:
-    #     rowz = csv.DictReader(csvfile)
-    #     for row in rowz:
-    #         line = str(row['time']) +  ' ' + str(row['flux']) + ' ' + str(row['flux_err'])
-    #         print(line)
-    # df = pd.read_csv('lightcurve.csv', header=None, index_col=0)
-    # # print(df)
-    # content = str(df)
-    # print(content)
-    # print(content, file=open('lightcurve.txt', 'w'))
-    # savedlc = open('light_curve.txt', 'wb')
-    # savedlc.write(lcf[int(sector_num.get())])
-    # savedlc.close()
+        # txtcurve = str(obj_name.get()) + '_tess.txt'
+        # file = open(txtcurve, 'w')
+        # for row in lcf[int(sector_num.get())]:
+        #     mag = -2.5 * float(row['flux']) + 20
+        #     line = str(row['time']) + ' ' + str(mag) + ' ' + str(row['flux_err']) + '\n'
+        #     # line = str(row['time']) + ' ' + str(row['flux']) + ' ' + str(row['flux_err']) + '\n'
+        #     # print(line)
+        #     file.write(line)
+        # file.close()
+
+        # with open('lightcurve.csv', newline='') as csvfile:
+        #     rowz = csv.DictReader(csvfile)
+        #     for row in rowz:
+        #         line = str(row['time']) +  ' ' + str(row['flux']) + ' ' + str(row['flux_err'])
+        #         print(line)
+        # df = pd.read_csv('lightcurve.csv', header=None, index_col=0)
+        # # print(df)
+        # content = str(df)
+        # print(content)
+        # print(content, file=open('lightcurve.txt', 'w'))
+        # savedlc = open('light_curve.txt', 'wb')
+        # savedlc.write(lcf[int(sector_num.get())])
+        # savedlc.close()
+        figx = (screen_x-558)/100
+        figy = (figx * 0.5625)
+        fig = plt.Figure(figsize=(figx, figy), dpi = 100)
+        #fig.add_subplot(111).plot(x, y, "ro")
+
     figx = (screen_x-558)/100
     figy = (figx * 0.5625)
     fig = plt.Figure(figsize=(figx, figy), dpi = 100)
-    #fig.add_subplot(111).plot(x, y, "ro")
-
-
     ax = fig.add_subplot(111)
     ax.plot(xx, yy, 'b', marker='o', linestyle='dashed', linewidth=1, markersize=4)
     # EXPERIMENTAL ROWS TO ADD FIT CURVE / ked bude treba
@@ -205,7 +222,7 @@ def curve_plot():
     # fig.add_subplot(111).plot(xx, yy, color='blue', marker='o', linestyle='dashed',
     #  linewidth=1, markersize=4)
     # BACKUP ROWS
-
+    fitted = False
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas.draw()
     canvas.get_tk_widget().pack()
@@ -230,11 +247,88 @@ def clear_JD():
     JDend_entered.delete(0, END)
 
 
+Gaussian = IntVar()
+Lorentzian = IntVar()
+checkboxGauss = tk.Checkbutton(master=frame1, text=' Gaussian', variable=Gaussian, onvalue=1, offvalue=0, bg="grey")
+checkboxGauss.place(x=395, y=120)
+checkboxLorentz = tk.Checkbutton(master=frame1, text=' Lorentzian', variable=Lorentzian, onvalue=1, offvalue=0, bg="grey")
+checkboxLorentz.place(x=395, y=140)
+
+def fitprocessing():
+    global ax
+    global fitted
+    global window
+
+    window = tk.Canvas(master=root, width=screen_x - 558, height=screen_y - 50, bg='white')
+    window.grid(row=0, column=1, sticky='N')
+
+    # x = lcf[int(sector_num.get())].time.value
+    # y = lcf[int(sector_num.get())].flux.value
+
+    fxx = []
+    fxx2 = []
+    fyy = []
+    invyy = []
+    fitt = []
+
+    if JDstart_entered.get() != '':
+        JDstart = float(JDstart_entered.get())
+        JDend = float(JDend_entered.get())
+        for i in range (0, len(x)):
+            if JDstart < x[i] and  x[i] < JDend:
+                fxx.append(float(x[i]))
+                fyy.append(float(y[i]))
+                invyy.append(1-float(y[i]))
+
+    Maxmagvalue = np.max(fyy, axis = 0)
+    Minmagvalue = np.min(fyy, axis = 0)
+
+    magscale = Maxmagvalue - Minmagvalue
+
+    fstart = float(JDstart_entered.get())  # getting user starting and ending point
+    fend = float(JDend_entered.get())      # of fitting
+
+    if Gaussian.get() == 1:  # fitting and drawing Gaussian model
+        sd = (fend - fstart) / 4
+        g_init = models.Gaussian1D(amplitude=magscale, mean=fxx[len(fxx) // 2], stddev=sd)
+        fit_g = fitting.LevMarLSQFitter()
+        fitted_g = fit_g(g_init, fxx, invyy)
+
+        for i in range (0, len(fxx)):
+            fitt.append(1-fitted_g(fxx[i]))
+
+    fitted = True
+    ax.cla()
+
+    figx = (screen_x - 558) / 100
+    figy = (figx * 0.5625)
+    fig = plt.Figure(figsize=(figx, figy), dpi=100)
+
+    ax = fig.add_subplot(111)
+    # ax.plot(xx, yy, 'b', fxx, fitt, 'r', marker='o', linestyle='dashed', linewidth=1, markersize=4)
+    ax.plot(xx, yy, 'b', marker='o', linestyle='dashed', linewidth=1, markersize=4)
+    ax.plot(fxx, fitt, 'r', linestyle='-', linewidth=2, markersize=4)
+    # ax.plot(fxx, fitt, 'r', xx, yy, 'b',  marker='o', linestyle='dashed', linewidth=1, markersize=4)
+    # ax.yaxis.set_major_locator(plt.NullLocator())
+    # ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+
+    canvas = FigureCanvasTkAgg(fig, master=window)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    toolbar = NavigationToolbar2Tk(canvas, window)
+    toolbar.update()
+    # # cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    canvas._tkcanvas.pack()
+
+
+fit_button = ttk.Button(frame1, text='Fit Selection', width = 11, command=fitprocessing)
+fit_button.place(x=400, y=170)
+
 clear_JD_button = ttk.Button(frame1, text='CLR', width = 4, command=clear_JD)
 clear_JD_button.place(x=220, y=75)
 
 targetJD = 'start'
-# global vlines
 vlines = 0
 
 
