4d39a6af3be3f87b921a723638b7b65381e3002f
Elexorr <ezofagus@gmail.com>
2024-05-15 07:15:42 +0200

    hello

--- nanovo.py
+++ nanovo.py
@@ -1,24 +1,29 @@
 import lightkurve as lk
-import numpy as np
+from lightkurve import search_targetpixelfile
 import matplotlib.pyplot as plt
+# from IPython.display import display
+# import IPython
+# IPython.embed()
 
-search_ffi = lk.search_tesscut('DI Cam')
-# search_tpf = lk.search_targetpixelfile('V523 Cas')
-# search_lcf = lk.search_lightcurve('V523 Cas')
+search_ffi = lk.search_tesscut('TIC 158554559')
 
-print(search_ffi)
-ffi_data = search_ffi[1].download(cutout_size=10)
+# print(search_ffi)
+ffi_data = search_ffi[1].download(cutout_size=21)
 
 # ffi_data.plot()
 
-target_mask = ffi_data.create_threshold_mask(threshold=50, reference_pixel='center')
-n_target_pixels = target_mask.sum()
-print(n_target_pixels)
-ffi_data.plot(aperture_mask=target_mask, mask_color='r')
+tpf = search_targetpixelfile('KIC 8462852', mission='Kepler', cadence='long').download()
+# target_mask = ffi_data.create_threshold_mask(threshold=5, reference_pixel='center')
+# n_target_pixels = target_mask.sum()
+# print(n_target_pixels)
+plt.figure()
+tpf.plot(interactive=True)
+# ffi_data.plot(interactive=True)
+# ffi_data.interact()
 
-ffi_lc = ffi_data.to_lightcurve(aperture_mask=target_mask)
-print(ffi_lc)
-ffi_lc.plot(label="SAP FFI")
+# ffi_lc = ffi_data.to_lightcurve(aperture_mask=target_mask)
+# print(ffi_lc)
+# ffi_lc.plot(label="SAP FFI")
 
 
 
